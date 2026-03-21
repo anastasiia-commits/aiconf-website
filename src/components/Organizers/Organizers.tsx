4ee23b249d9f4597e4f48f5b81c67ddb7b6bdac6
Anastasiia Prokopenko <anastasiia@tripleten.com>
2026-03-21 11:00:31 +0100

    pictures upd-3

--- src/components/Organizers/Organizers.tsx
+++ src/components/Organizers/Organizers.tsx
@@ -1,9 +1,11 @@
 import { useScrollReveal } from '../../hooks/useScrollReveal.ts'
 import './Organizers.css'
+import efimPhoto from '../../../public/Efim_Boieru.jpg'
+import anastasiaPhoto from '../../../public/Anastasiia_Prokopenko.jpg'
 
 const organizers = [
-  { initials: 'EB', name: 'Efim Boieru', role: 'Technical Vision & Content', linkedin: 'https://www.linkedin.com/in/efimboieru/', photo: '/Users/nastyaapro/Desktop/aiconf-website/public/Efim_Boieru.jpg' },
-  { initials: 'AP', name: 'Anastasiia Prokopenko', role: 'Strategy & Partnerships', linkedin: 'https://www.linkedin.com/in/anastasiia-prokopenko/', photo: '/Users/nastyaapro/Desktop/aiconf-website/public/Efim_Boieru.jpg' },
+  { initials: 'EB', name: 'Efim Boieru', role: 'Technical Vision & Content', linkedin: 'https://www.linkedin.com/in/efimboieru/', photo: efimPhoto },
+  { initials: 'AP', name: 'Anastasiia Prokopenko', role: 'Strategy & Partnerships', linkedin: 'https://www.linkedin.com/in/anastasiia-prokopenko/', photo: anastasiaPhoto },
   { initials: 'MG', name: 'Mikhail Gutnikov', role: 'Operations & Growth', linkedin: 'https://www.linkedin.com/in/mikhail-gutnikov-b662611b5/', photo: null },
 ]
 
