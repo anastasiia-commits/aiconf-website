27c8b62e48032627f01f4cdf809327d71b152a78
Anastasiia Prokopenko <anastasiia@tripleten.com>
2026-03-21 10:57:33 +0100

    pictures upd-2

--- src/components/Organizers/Organizers.tsx
+++ src/components/Organizers/Organizers.tsx
@@ -2,16 +2,19 @@ import { useScrollReveal } from '../../hooks/useScrollReveal.ts'
 import './Organizers.css'
 
 const organizers = [
-  { initials: 'EB', name: 'Efim Boieru', role: 'Technical Vision & Content', linkedin: 'https://www.linkedin.com/in/efimboieru/' },
-  { initials: 'AP', name: 'Anastasiia Prokopenko', role: 'Strategy & Partnerships', linkedin: 'https://www.linkedin.com/in/anastasiia-prokopenko/' },
-  { initials: 'MG', name: 'Mikhail Gutnikov', role: 'Operations & Growth', linkedin: 'https://www.linkedin.com/in/mikhail-gutnikov-b662611b5/' },
+  { initials: 'EB', name: 'Efim Boieru', role: 'Technical Vision & Content', linkedin: 'https://www.linkedin.com/in/efimboieru/', photo: '/Users/nastyaapro/Desktop/aiconf-website/public/Efim_Boieru.jpg' },
+  { initials: 'AP', name: 'Anastasiia Prokopenko', role: 'Strategy & Partnerships', linkedin: 'https://www.linkedin.com/in/anastasiia-prokopenko/', photo: '/Users/nastyaapro/Desktop/aiconf-website/public/Efim_Boieru.jpg' },
+  { initials: 'MG', name: 'Mikhail Gutnikov', role: 'Operations & Growth', linkedin: 'https://www.linkedin.com/in/mikhail-gutnikov-b662611b5/', photo: null },
 ]
 
 function OrgCard({ org }: { org: typeof organizers[number] }) {
   const ref = useScrollReveal<HTMLAnchorElement>()
   return (
     <a ref={ref} className="org-card" href={org.linkedin} target="_blank" rel="noreferrer">
-      <div className="org-avatar">{org.initials}</div>
+      {org.photo
+        ? <div className="org-photo-wrap"><img className="org-photo" src={org.photo} alt={org.name} /></div>
+        : <div className="org-avatar">{org.initials}</div>
+      }
       <h4>{org.name}</h4>
       <p>{org.role}</p>
       <span className="org-link mono">linkedin ↗</span>
